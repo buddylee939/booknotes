13e8c3e5f298d9aebf0f38b7f22753cfcc67f745
Jose merconchini <buddylee939@hotmail.com>
2018-05-15 18:23:11 -0500

    Up to chapter 5 in dangerous, created a word count program

--- js/learnenough.js
+++ js/learnenough.js
@@ -51,3 +51,107 @@ console.log(totalArr === total);
 // var dayname = daysOfTheWeek[now.getDay()];
 // alert(`Hello, world! Happy ${dayname}.`);
 
+const sonnet = `Let me not to the marriage of true minds
+Admit impediments. Love is not love
+Which alters when it alteration finds,
+Or bends with the remover to remove.
+O no, it is an ever-fixed mark
+That looks on tempests and is never shaken;
+It is the star to every wand'ring bark,
+Whose worth's unknown, although his height be taken.
+Love's not time's fool, though rosy lips and cheeks
+Within his bending sickle's compass come:
+Love alters not with his brief hours and weeks,
+But bears it out even to the edge of doom.
+  If this be error and upon me proved,
+  I never writ, nor no man ever loved.`;
+
+// Unique words
+let uniques = {};
+// All words in the text
+let reg = /[\w]+/g;
+let words = sonnet.match(reg);
+// console.log(words.length)
+// Iterate through `words` and build up an associative array of unique words.
+for (let i = 0; i < words.length; i++) {
+  let word = words[i];
+  if (uniques[word]) {
+    uniques[word] += 1;
+  } else {
+    uniques[word] = 1;
+  }
+}
+
+console.log(uniques)
+
+// > let uniques = new Map();
+// Map {}
+// > uniques.set("loved", 0);
+// Map { 'loved' => 0 }
+// > let currentValue = uniques.get("loved");
+// > uniques.set("loved", currentValue + 1);
+// Map { 'loved' => 1 }
+
+let uniquesMap = new Map();
+// let reg = /[\w]+/g;
+// let words = sonnet.match(reg);
+for (let i = 0; i < words.length; i++) {
+	let word = words[i];
+	if (uniquesMap.get(word)){
+		let currentValue = uniquesMap.get(word)
+		uniquesMap.set(word, currentValue + 1)
+	} else {
+		uniquesMap.set(word, 1)
+	}
+}
+console.log(uniquesMap);
+
+
+function sortWords(object){
+	var maxSpeed = {
+	    car: 300, 
+	    bike: 60, 
+	    motorbike: 200, 
+	    airplane: 1000,
+	    helicopter: 400, 
+	    rocket: 8 * 60 * 60
+	};
+	var sortable = [];
+	for (var newWord in object) {
+	    sortable.push([newWord, object[newWord]]);
+	}
+
+	sortable.sort(function(a, b) {
+	    return a[1] - b[1];
+	});
+
+	console.log('There are now ' + sortable.length + ' unique words in that text');
+	console.log(sortable.reverse());	
+}
+
+function uniqueWord(textarea){	
+	let uniquesW = {};
+	let regex = /[\w'-]+/g;
+
+	let wordsNew = textarea.match(regex);
+	for (let i = 0; i < wordsNew.length; i++) {
+	  let word = wordsNew[i].toLowerCase();
+	  if (uniquesW[word]) {
+	    uniquesW[word] += 1;
+	  } else {
+	    uniquesW[word] = 1;
+	  }
+	}
+	// console.log(uniquesW);	
+	return uniquesW;
+}
+
+$('#btnSubmit').click(function(e){
+	var wordObject = {};
+	e.preventDefault();
+	var textarea = $('#textarea').val();
+	wordObject = uniqueWord(textarea);
+	// console.log(wordObject);
+	sortWords(wordObject);
+});
+
